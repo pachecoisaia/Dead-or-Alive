ac325a5b28c551fcd2e128f3c2b3d102593fe16a
isaia pacheco <ipacheco9311@sdsu.edu>
2020-12-08 14:21:26 -0800

    third person camera and movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody rb;
+    //public Rigidbody rb;
     public float moveSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        //rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //player movement
-        float xMov = Input.GetAxisRaw("Horizontal");
-        float zMov = Input.GetAxisRaw("Vertical");
+        float xMov = Input.GetAxis("Horizontal");
+        float zMov = Input.GetAxis("Vertical");
+	
+	    Vector3 Movement = new Vector3(xMov, 0f, zMov) * moveSpeed * Time.deltaTime;
+	    transform.Translate(Movement, Space.Self);
 
-        rb.velocity = new Vector3(xMov, rb.velocity.y, zMov) * moveSpeed;
+        //rb.velocity = new Vector3(xMov, rb.velocity.y, 		//zMov) * moveSpeed;
     }
 
 }
