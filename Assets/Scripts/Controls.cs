50b8e243823519d92452dd220cda2c13bab6001d
Isaac Pacheco <ipacheco3354@sdsu.edu>
2020-12-10 13:33:52 -0800

    fixed erros on game scene, finalized ui, fixed buttons, fixed game scene errors

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+public class Controls : MonoBehaviour
+{
+    public Button backButton;
+    public AudioSource audio;
+
+    void Start()
+    {
+        backButton = GameObject.FindGameObjectWithTag("BackButton").GetComponent<Button>();
+        backButton.onClick.AddListener(Back);
+
+        audio = GameObject.FindGameObjectWithTag("Audio3").GetComponent<AudioSource>();
+        audio.Play();
+    }
+
+    public void Back()
+    {
+        Debug.Log("Back");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
