98f8412168f98c4878a5c46d279b32838a6edd25
isaia pacheco <ipacheco9311@sdsu.edu>
2020-12-09 17:05:25 -0800

    added about scene

---
+++ Assets/Scripts/About.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+public class About : MonoBehaviour
+{
+    public Button backButton;
+    public AudioSource audio;
+
+    void Start()
+    {
+        backButton = GameObject.FindGameObjectWithTag("BackButton").GetComponent<Button>();
+		backButton.onClick.AddListener(Back);
+
+        audio = GameObject.FindGameObjectWithTag("Audio2").GetComponent<AudioSource>();
+        audio.Play();
+    }
+
+    public void Back()
+    {
+        Debug.Log("Back");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - SceneManager.GetActiveScene().buildIndex);
+    }
+}
