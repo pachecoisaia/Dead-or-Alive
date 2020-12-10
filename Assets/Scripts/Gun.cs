c81a8d6d856b1b03798257cc5d84d08f4afc50ff
isaia pacheco <ipacheco9311@sdsu.edu>
2020-12-10 01:50:41 -0800

    switch weapons added sounds

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,7 +6,7 @@ public class Gun : MonoBehaviour
 {
     [SerializeField]
     [Range(0.0f, 1.5f)]
-    private float fireRate = 1f;
+    private float fireRate = .5f;
     [SerializeField]
     [Range(1, 10)]
     private int damage = 1;
@@ -15,8 +15,11 @@ public class Gun : MonoBehaviour
     private Transform firePoint;
     //private ParticleSystem ps;
 
-
+    public GameObject mp5, ak47, m4;
+    private int indexOfSelectedGun = 0;
     private float timer;
+
+    public AudioSource[] gunSounds;
 
     void Start()
     {
@@ -34,10 +37,38 @@ public class Gun : MonoBehaviour
                 timer = 0f;
                 FireGun();
             }
+
+            if (Input.GetKey(KeyCode.Alpha1)) {
+                mp5.SetActive(true);
+                ak47.SetActive(false);
+                m4.SetActive(false);
+                indexOfSelectedGun = 0;
+                fireRate = .5f;
+                damage = 4;
+                
+
+            }
+            if (Input.GetKey(KeyCode.Alpha2)) {
+                mp5.SetActive(false);
+                ak47.SetActive(true);
+                m4.SetActive(false);
+                indexOfSelectedGun = 1;
+                fireRate = .33f;
+                damage = 5;
+            }
+            if (Input.GetKey(KeyCode.Alpha3)) {
+                mp5.SetActive(false);
+                ak47.SetActive(false);
+                m4.SetActive(true);
+                indexOfSelectedGun = 2;
+                fireRate = .1f;
+                damage = 10;
+            }
         }
     }
     void FireGun()
     {
+        PlayGunSound();
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         Debug.DrawRay(ray.origin, ray.direction * 10000f, Color.red);
 
@@ -52,6 +83,8 @@ public class Gun : MonoBehaviour
             }
         }
     }
+    
+    void PlayGunSound() {
+        gunSounds[indexOfSelectedGun].Play();
+    }
 }
-
-
